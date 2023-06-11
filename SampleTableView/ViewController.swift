2d0ab088d45555f529229eeefd24f048a7da65db
taiga07 <taigatani@me.com>
2023-06-11 22:47:41 +0900

    first commit

--- SampleTableView/ViewController.swift
+++ SampleTableView/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let fruits = ["りんご","みかん","ブドウ","レモン","スイカ"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel!.text = fruits[indexPath.row]
+        
+        return cell
+    }
 
 }
 
